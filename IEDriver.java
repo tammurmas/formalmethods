4e19533763b9e7688b1224b11acd9262c12e729f
tammurmas <tammurmas@gmail.com>
2013-12-28 23:25:43 +0200

    IDE tests

--- IEDriver.java
+++ IEDriver.java
@@ -1,26 +1,36 @@
 package formalmethods;
 
+import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
-public class FormalMethods {
+public class IEDriver {
 
-    public static void main(String[] args) throws NoSuchElementException {
+    private static String driverLocation = "E:/selenium-2.39.0/IEDriverServer_x64_2.39.0/IEDriverServer.exe";
     
+    public static void main(String[] args) throws NoSuchElementException {    
+        
+        File file = new File(driverLocation);
+        System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
+        
         //set the logger to the highest level to avoid unneeded warnings
         Logger logger = Logger.getLogger ("");
         logger.setLevel (Level.SEVERE);
         
-        // Create a new instance of the html unit driver
-        // Notice that the remainder of the code relies on the interface, 
-        // not the implementation.
-        WebDriver driver = new HtmlUnitDriver();
+        // Create a new instance of IE driver
+        //WebDriver driver = new InternetExplorerDriver();
+        
+        // Create a new instance of Firefox driver
+        WebDriver driver = new FirefoxDriver();
 
         // And now use this to visit Google
         driver.get("http://www.google.com");
@@ -38,6 +48,12 @@ public class FormalMethods {
         try
         {
             String linkName = "Formal methods - Wikipedia, the free encyclopedia";
+            
+            // create anew WebDriverWait object from the previous "regular" driver
+            // wait until the page is loaded and link is clickable
+            WebDriverWait wait = new WebDriverWait(driver, 5);
+            wait.until(ExpectedConditions.elementToBeClickable(By.linkText(linkName)));
+            
             WebElement link = driver.findElement(By.linkText(linkName));
             //click the link
             link.click();
@@ -56,5 +72,6 @@ public class FormalMethods {
         {
             driver.quit();
         }
+        
     }
 }
